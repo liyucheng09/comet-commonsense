700ae959983d8fb427ff1025e2b88082d34a9af0
yucheng lab <liyucheng_2@qq.com>
2022-01-24 19:39:40 +0000

    edit codecs

--- lm_pipeline.py
+++ lm_pipeline.py
@@ -1,5 +1,7 @@
 from transformers import BertTokenizer, GPT2LMHeadModel, TextGenerationPipeline
 import sys
+import codecs
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
 if __name__ == '__main__':
 
